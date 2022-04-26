a330e398c9fc1a116f8769241808182bf23b1b96
bryangra <41131495+bryangra@users.noreply.github.com>
2022-04-25 20:48:44 -0700

    Initial Event Commit

---
+++ src/distributed-file-system/src/csueb/cs401/common/Event.java
@@ -0,0 +1,30 @@
+java.util.Date;
+
+public class Event {
+
+	private Date eventDate;
+	private String eventDescription;
+	private String userID;
+	
+	public boolean createEvent(String inDescription, String inUser) {
+		
+		
+		return false;
+	}
+	
+	public String getEvent() {
+		String output = new String();
+		
+		return output;
+	}
+	
+	public boolean deleteEvent() {
+		
+		return false;
+	}
+	
+	public boolean overrideEvent(String newDescription, String newUser, Date newDate) {
+		
+		return false;
+	}
+}
