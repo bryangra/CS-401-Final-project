1e70e72d6c3f1a7ea7c35a94b64864bfc07c04d4
bryangra <41131495+bryangra@users.noreply.github.com>
2022-04-25 21:15:23 -0700

    Event Code

--- src/distributed-file-system/src/csueb/cs401/common/Event.java
+++ src/distributed-file-system/src/csueb/cs401/common/Event.java
@@ -1,4 +1,7 @@
-java.util.Date;
+package csueb.cs401.common;
+
+import java.util.Date;
+import java.util.UUID;
 
 public class Event {
 
@@ -6,25 +9,93 @@ public class Event {
 	private String eventDescription;
 	private String userID;
 	
-	public boolean createEvent(String inDescription, String inUser) {
-		
-		
-		return false;
+	
+	
+	//******************************************************
+	// Default constructor
+	//*******************************************************
+	public Event() {
+		eventDate.toInstant();
+		eventDescription = new String("");
+		userID = new String("");
 	}
 	
+	
+	
+	
+	//******************************************************
+	// Creates an event with the description and user given
+	// Input: string description of event and string userID 
+	// Output: an event set to current date and time with
+	// values specified.
+	//*******************************************************
+	public Event(String inDescription, String inUser) {
+		eventDate.toInstant();
+		this.eventDescription = inDescription;
+		this.userID = inUser;
+	}
+	
+	
+	
+	//******************************************************
+	// Creates an event with the description and user given
+	// Input: string description of event and string userID 
+	// Output: bool weather event was created and an event 
+	// set to current date and time with values specified.
+	//*******************************************************
+	public boolean createEvent(String inDescription, String inUser) {
+		eventDate.toInstant();
+		this.eventDescription = inDescription;
+		this.userID = inUser;
+		
+		return true;
+	}
+	
+	
+	
+	//******************************************************
+	// Returns event in string format
+	// Input: none 
+	// Output: string Description, User, Date and Time
+	//*******************************************************
 	public String getEvent() {
 		String output = new String();
+		
+		output = "Desc:" + this.eventDescription + ", User:" +
+				userID + ", Date:" + eventDate.toString();
 		
 		return output;
 	}
 	
+	
+	
+	//******************************************************
+	// Deletes event information
+	// Input: none 
+	// Output: boolean true if info was cleared, date to time
+	// of clearing
+	//*******************************************************
 	public boolean deleteEvent() {
+		eventDate.toInstant();
+		eventDescription = new String("");
+		userID = new String("");
 		
-		return false;
+		return true;
 	}
 	
-	public boolean overrideEvent(String newDescription, String newUser, Date newDate) {
+	
+	
+	//******************************************************
+	// overwrites and event event information
+	// Input: none 
+	// Output: boolean true if info was cleared, date to time
+	// of clearing
+	//*******************************************************
+	public boolean overwriteEvent(String newDescription, String newUser, Date newDate) {
+		eventDate = newDate;
+		this.eventDescription = newDescription;
+		this.userID = newUser;
 		
-		return false;
+		return true;
 	}
 }
--- src/distributed-file-system/src/csueb/cs401/persist/Logger.java
+++ src/distributed-file-system/src/csueb/cs401/persist/Logger.java
@@ -1,5 +1,10 @@
 package csueb.cs401.persist;
 
+import java.util.Date;
+
 public class Logger {
+	
+
+
 
 }
